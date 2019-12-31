d2bdb5466d2359b941c34b1cf73b9fdf45212895
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2019-12-31 19:51:48 +1300

    Added constants in run function; began instuctions

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+//
+#                                                                                                                                       //
+# main.py                                                                                                                               //
+#                                                                                                                                       //
+# D. C. Groothuizen Dijkema - December, 2019                                                                                            //
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+//
+
 from time import process_time as time
 import errno
 import os
@@ -7,16 +14,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from huygens.plotting import simulation_plot,setup_simulation_plot
-from frog import simulate,harmonic
+from frog import simulate,probability,harmonic,simulate_py
 
+out_dir='./out/' # change if you want the plots somewhere else
 try:
-  os.makedirs('./out/')
+  os.makedirs(out_dir) 
 except OSError as err:
   if err.errno != errno.EEXIST:
     raise
 
+'''
+============
+Instructions
+============
+
+This Python script contains two functions, one for approximating a given harmonic number and one for comparing the speeds of Python and
+compiled C++.
+
+'''
+
 def run():
-  pass
+  num_pads=10
+  num_itr=1000000
+  num_runs=150
+  run_size=10
 
 def time_comparison():
   pass
